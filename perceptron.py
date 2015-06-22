18e49af7b9d73775db59fd2d121b97fb2645996e
Jeff Schluep <jlsiv1@mac.com>
2015-06-22 15:15:13 -0400

    Added feedfoward and activate

--- perceptron.py
+++ perceptron.py
@@ -8,3 +8,11 @@ class perceptron:
         :return: a perceptron
         '''
         self.weights = [random.uniform(-1,1) for i in range(n)]
+    def activate(self, sum):
+        if sum > 0: return 1
+        else: return -1
+    def feedfoward(self, inputs):
+        sum = 0.0
+        for i in range(len(inputs)):
+            sum += self.weights[i]*inputs[i]
+        return self.activate(sum)
